9b17079d30f4d64189c5c3854aeddc6a5b14042c
SamrawitDawit <samrikdawit@gmail.com>
2024-08-10 13:35:27 +0300

    made improvements on scan

--- task1/task1-student-grade-calculator.go
+++ task1/task1-student-grade-calculator.go
@@ -1,15 +1,20 @@
 // Student grade calculator
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-//defining data type to store student data
+// defining data type to store student data
 type student struct {
 	name              string
 	subject_and_grade map[string]float64
 }
 
-//defining a method to calculate average grade
+// defining a method to calculate average grade
 func (s student) averageGrade() float64 {
 	var sum float64
 	for _, grade := range s.subject_and_grade {
@@ -19,28 +24,36 @@ func (s student) averageGrade() float64 {
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	test()
 	//Implementing a loop to ask for student data
 	var studentName string
 	fmt.Println("Enter your name: ")
-	fmt.Scanln(&studentName)
+	scanner.Scan()
+	studentName = scanner.Text()
 	studentData := student{
 		name:              studentName,
 		subject_and_grade: make(map[string]float64),
 	}
 	var numSubjects int
 	fmt.Println("Enter number of subjects: ")
-	fmt.Scanln(&numSubjects)
+	scanner.Scan()
+	numSubjects, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		fmt.Println("Invalid input for number of subjects.")
+		return
+	}
 	for j := 0; j < numSubjects; j++ {
 		var subject string
 		fmt.Println("Enter subject: ")
-		fmt.Scanln(&subject)
+		scanner.Scan()
+		subject = scanner.Text()
 		var grade float64
 		fmt.Println("Enter grade: ")
-		fmt.Scanln(&grade)
-		//validating the grade
-		if grade < 0 || grade > 100 {
-			fmt.Println("Invalid grade.")
+		scanner.Scan()
+		grade, err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil || grade < 0 || grade > 100 {
+			fmt.Println("Invalid input for grade.")
 			return
 		}
 		//storing the student data
@@ -55,7 +68,7 @@ func main() {
 	fmt.Printf("Average Grade: %.2f\n", studentData.averageGrade())
 }
 
-//testing the code
+// testing the code
 func test() {
 	//Test case 1
 	studentData := student{
