3f6c971312e9080330e83105604d4f21fb42a9ba
SamrawitDawit <samrikdawit@gmail.com>
2024-08-16 11:06:01 +0300

    Update test repo to use embedded MongoDB

--- task8-Testing/Repositories/user_repositories_test.go
+++ task8-Testing/Repositories/user_repositories_test.go
@@ -2,36 +2,81 @@ package repositories
 
 import (
 	"context"
-	"log"
+	"fmt"
 	domain "task8-Testing/Domain"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type userRepositorySuite struct {
 	suite.Suite
 	repository domain.UserRepository
+	mongoC     testcontainers.Container
+	client     *mongo.Client
 }
 
-func (suite *userRepositorySuite) SetupSuite() {
-	clientOption := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOption)
+func (suite *userRepositorySuite) setupMongoContainer() (testcontainers.Container, *mongo.Client) {
+	ctx := context.Background()
+
+	mongoC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "mongo:latest",
+			ExposedPorts: []string{"27017/tcp"},
+			WaitingFor:   wait.ForListeningPort("27017/tcp"),
+		},
+		Started: true,
+	})
 	if err != nil {
-		log.Fatal(err)
+		suite.T().Fatalf("could not start mongo container: %v", err)
 	}
+	host, err := mongoC.Host(ctx)
+	if err != nil {
+		suite.T().Fatalf("could not get mongo container host: %v", err)
+	}
+	port, err := mongoC.MappedPort(ctx, "27017")
+	if err != nil {
+		suite.T().Fatalf("could not get mongo container port : %v", err)
+	}
+
+	mongoURI := fmt.Sprintf("mongodb://%s:%s", host, port.Port())
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+
+	if err != nil {
+		suite.T().Fatalf("couldn't connect to mongo: %v", err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		suite.T().Fatalf("could not ping mongo: %v", err)
+	}
+	return mongoC, client
+}
+func (suite *userRepositorySuite) SetupSuite() {
+	mongoC, client := suite.setupMongoContainer()
 	db := client.Database("test_db")
-	repo := NewUserRepository(*db, "users")
+	repo := NewUserRepository(*db, "tasks")
 	suite.repository = repo
+	suite.mongoC = mongoC
+	suite.client = client
 }
 
 func (suite *userRepositorySuite) TearDownTest() {
-	suite.repository.(*userRepository).database.Collection("users").Drop(context.Background())
+	err := suite.client.Disconnect(context.Background())
+	if err != nil {
+		suite.T().Fatalf("couldn't disconnect from mongo: %v", err)
+	}
+	err = suite.mongoC.Terminate(context.Background())
+	if err != nil {
+		suite.T().Fatalf("couldn't terminate mongo container: %v", err)
+	}
 }
-
 func (suite *userRepositorySuite) TestRegister_Positive() {
 	user := domain.User{
 		Username: "samrawit",
